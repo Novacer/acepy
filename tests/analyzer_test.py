79a1be931b2adeeb22da06361e09e0a87deb5e37
jack <zhangjack27@gmail.com>
2019-12-26 16:53:17 -0800

    analyzer handles string annotations

--- tests/analyzer_test.py
+++ tests/analyzer_test.py
@@ -5,15 +5,17 @@ from src.analyzer import *
 
 class AnalyzerTest(unittest.TestCase):
 
-    one_function = '''def f(x: param) -> result: return x'''
+    function_str = '''def f(x: 'param') -> 'result': return x'''
+    function_named = '''def f(x: param) -> result: return x'''
 
     def test_analyze(self):
-        tree = parse(self.one_function)
-        dependencies = analyze(tree)
-        self.assertIn('f', dependencies)
-        dependency_data = dependencies['f']
-        self.assertIn(('x', 'param'), dependency_data.dependencies)
-        self.assertEqual('result', dependency_data.returns)
+        for function in [self.function_str, self.function_named]:
+            tree = parse(function)
+            dependencies = analyze(tree)
+            self.assertIn('f', dependencies)
+            dependency_data = dependencies['f']
+            self.assertIn(('x', 'param'), dependency_data.dependencies)
+            self.assertEqual('result', dependency_data.returns)
 
 
 if __name__ == '__main__':
