46019bbe6f4ec714c7976bfb91f0243e0a3ac2e7
jack <zhangjack27@gmail.com>
2019-12-28 16:49:22 -0800

    implement validation for analyzer

--- tests/analyzer_test.py
+++ tests/analyzer_test.py
@@ -1,6 +1,34 @@
 import unittest
 from src.analyzer import *
 
+unique_returns = '''
+def f(x) -> a:
+    return 1
+
+def g(x):
+    return 1
+
+def h(x) -> b:
+    return 1
+
+def a(x):
+    return 1
+
+def b(x) -> c:
+    return 1
+
+'''
+
+duplicate_returns = unique_returns + '''
+def c(x) -> a:
+    return 1
+'''
+
+function_overloading = unique_returns + '''
+def f():
+    return 1
+'''
+
 
 class AnalyzerTest(unittest.TestCase):
     function_str = '''def f(x: 'param') -> 'result': return x'''
@@ -20,6 +48,16 @@ class AnalyzerTest(unittest.TestCase):
         dependencies = analyze(parse(self.function_no_annotation))
         self.assertIn('f', dependencies)
         self.assertListEqual(dependencies['f'].dependencies, [])
+
+    def test_returns_are_unique(self):
+        dep = analyze(parse(unique_returns))
+        self.assertTrue(returns_are_unique(dep))
+        dep = analyze(parse(duplicate_returns))
+        self.assertFalse(returns_are_unique(dep))
+
+    def test_overload_error(self):
+        tree = parse(function_overloading)
+        self.assertRaises(FunctionOverloadError, analyze, tree)
 
 
 if __name__ == '__main__':
